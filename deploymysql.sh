7041064cd8a97bcd595c375d9c2ce9e40a50c68c
Your Name <you@example.com>
2019-04-19 15:26:26 +0800

    mysql install scripts

---
+++ deploymysql.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# Author: bavdu
+# Email: bavduer@163.com
+# Date: 2019/04/19
+# Usage: deploy mysql source installstion.
+
+yum -y groupinstall "Development Tools"
+yum -y install ncurses ncurses-devel bison libgcrypt perl make cmake
+
+if id mysql;then
+  echo "mysql user is exists."
+else
+  groupadd mysql && useradd -M -g mysql -s /sbin/nologin mysql
+fi
+
+tar xf $1 -C /usr/local/
+
+chown -R mysql.mysql /usr/local/mysqld
+echo "export PATH=$PATH:/usr/local/mysqld/mysql/bin" >>/etc/profile
+source /etc/profile
+
+rm -rf /etc/my.cnf
+cp -f /usr/local/mysqld/my.cnf /etc/my.cnf
+
+cp -f /usr/local/mysqld/mysql/support-files/mysql.server /etc/init.d/mysqld
+chkconfig --add mysqld
+chkconfig mysqld on
